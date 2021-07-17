7b5549c7e877d958beeab451236a8faab8f9ffa3
rustyspoon71 <73858926+rustyspoon71@users.noreply.github.com>
2021-07-17 12:45:08 -0400

    Delete Complete_tutorial directory

--- Complete_tutorial/problem_1.py
+++
@@ -1,9 +0,0 @@
-# Tell me who was at party_1 but not at party_2. Then create a new set containing
-# the people who didnt go to party 2 and the people who didn't go to party_1. Then
-# output the set to know who wasn't at both parties. 
-
-party_1 = {"Annie","Richie","Rosie","Becca","Ryland","Scott"}
-party_2 = {"Annie","Richie","Rosie","Becca","Samantha","Mom","Dad"}
-
-# Put your code here you may use any type of operations.
-
